19372b84cb7506d88fd2f91b315095f73dce8f0f
unknown <zjh2642532007@163.com>
2025-11-11 22:12:17 +0800

    Git LFS commit

---
+++ 软件代码.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e2be9903e1824c22b948f1c436d729b355b21ad695633ceb09dbb58f33c8dfe
+size 36258
---
+++ 软件代码.spec
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d54ba6b9e115f030e82eeaf0af813e27c284047930378af8ab9dce0ae02e78d
+size 710
